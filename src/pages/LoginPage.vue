7039e23507e7ef4e43d5966ee03f531fddfd06f7
Anderson Patricio <andersongpatricio@hotmail.com>
2026-01-06 17:50:02 -0300

    feat: página de login

---
+++ src/pages/LoginPage.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-container class="pa-0 h-100" fluid>
+    <v-row no-gutters class="h-100">
+      <v-col class="d-flex align-center justify-center bg-grey-lighten-4" cols="12" md="6">Foto
+      </v-col>
+      <v-col class="d-flex align-center justify-center" cols="12" md="6">
+        <div class="w-100" style="max-width: 400px;">
+          <v-form>
+            <div class="text-center mb-6">
+              <h1>Login</h1>
+              <p>Preencha seus dados para logar</p>
+            </div>
+
+            <v-text-field l label="Email" required variant="outlined" type="email"></v-text-field>
+
+            <v-text-field label="Senhas" required variant="outlined" type="password"></v-text-field>
+
+            <v-btn block class="mb-3" color="secondary" size="large" type="submit">
+              Entrar
+            </v-btn>
+
+            <div class="text-center mb-3">
+              ou
+            </div>
+
+            <v-btn block variant="outlined" class="mb-3" color="primary" size="large" type="submit">
+              Cadastre-se
+            </v-btn>
+          </v-form>
+        </div>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -2,17 +2,29 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [{
+  routes: [
+    {
+      path: '/',
+       component: () => import('@/layouts/LoginLayout.vue'),
+      children: [
+        {
+          path: '',
+          name: 'login ',
+          component: () => import('@/pages/LoginPage.vue')
+        }
+      ]
+    },
+    {
     path: '/',
     component: () => import('@/layouts/DefaultLayout.vue'),
     children: [
       {
-        path: '',
+        path: 'home',
         name: 'home',
         component: () => import('@/pages/HomePage.vue')
       },
       {
-        path: '/category',
+        path: 'category',
         name: 'category',
         component: () => import('@/pages/CategoryPage.vue')
       },
